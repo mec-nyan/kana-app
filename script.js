43372d3a603022e3626b3434534eb876c919ed19
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-06-23 20:46:22 -0300

    First playable version!

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ progress.appendChild(bar)
 
 top_info.appendChild(progress)
 
-const score = 1000
+let score = 0
 
 const user_score = document.createElement("div")
 user_score.id = "score"
@@ -73,14 +73,54 @@ kana.innerText = kana_map[0].kanas[3].hiragana
 const romaji_bar = document.createElement("div")
 romaji_bar.id = "romaji-bar"
 
-const romaji = ["a", "i", "u", "e", "o"]
-for (let i = 0; i < romaji.length; i++) {
-	let btn = document.createElement("div")
-	btn.className = "romaji-button"
-	btn.innerText = romaji[i]
-	romaji_bar.appendChild(btn)
+let last_kana = 0
+
+const randomKana = (row, last) => {
+	while (true) {
+		const i = Math.floor(Math.random() * row.length)
+		if (i !== last) {
+			last_kana = i
+			return row[i]
+		}
+	}
 }
 
+const nextQuest = () => {
+	let row = kana_map[0].kanas
+	let current_kana = randomKana(row, last_kana)
+	kana.textContent = current_kana.hiragana
+
+	romaji_bar.innerHTML = ""
+	row.forEach(k => {
+		let btn = document.createElement("div")
+		btn.className = "romaji-button"
+		btn.innerText = k.romaji
+		btn.addEventListener("click", () => handleClick(k.romaji, current_kana))
+		btn.addEventListener('touchstart', () => {
+			btn.classList.add('touched')
+		})
+
+		btn.addEventListener('touchend', () => {
+			btn.classList.remove('touched')
+		})
+
+		btn.addEventListener('touchcancel', () => {
+			btn.classList.remove('touched')
+		})
+		romaji_bar.appendChild(btn)
+	})
+}
+
+const handleClick = (rmj, current) => {
+	const right = rmj === current.romaji
+	if (right) {
+		score++
+		user_score.innerHTML = `<span>Score: <span class='score'>${score}</span></span>`
+	}
+	nextQuest()
+}
+
+nextQuest()
 // Footer.
 const footer = document.createElement("div")
 footer.id = "footer"
