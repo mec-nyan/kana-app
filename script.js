7801a0131b905819269de4eee08caff1bcfe15d0
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-06-25 19:45:12 -0300

    Shuffle all rows!

--- script.js
+++ script.js
@@ -1,57 +1,56 @@
 // We'll be manipulating this div.
-const root = document.getElementById("root")
+const root = document.getElementById("root");
 
 // At the top, we'll place a div with general info.
 // Use small letters, like a hw monitor, etc.
-const top_info = document.createElement("div")
-top_info.id = "top-info"
+const top_info = document.createElement("div");
+top_info.id = "top-info";
 
-const mode = document.createElement("div")
-mode.id = "mode"
-mode.className = "info"
-mode.innerHTML = `<span>Mode: ひらがな (hiragana)</span>`
+const mode = document.createElement("div");
+mode.id = "mode";
+mode.className = "info";
+mode.innerHTML = `<span>Mode: ひらがな (hiragana)</span>`;
 
-top_info.appendChild(mode)
+top_info.appendChild(mode);
 
-const other_stuff = document.createElement("div")
-other_stuff.id = "other_stuff"
-other_stuff.className = "info"
-other_stuff.innerHTML = `<span>I may place some stats here.</span>`
+const other_stuff = document.createElement("div");
+other_stuff.id = "other_stuff";
+other_stuff.className = "info";
+other_stuff.innerHTML = `<span>I may place some stats here.</span>`;
 
-top_info.appendChild(other_stuff)
+top_info.appendChild(other_stuff);
 
-const progress = document.createElement("div")
-progress.id = "progress"
+const progress = document.createElement("div");
+progress.id = "progress";
 
-let percentage = 0
+let percentage = 0;
 
-const progress_tag = document.createElement("div")
-progress_tag.className = "info"
-progress_tag.innerHTML = `<span>Progress:&nbsp;<span class='perc'>${percentage}%</span></span>`
+const progress_tag = document.createElement("div");
+progress_tag.className = "info";
+progress_tag.innerHTML = `<span>Progress:&nbsp;<span class='perc'>${percentage}%</span></span>`;
 
-const bar = document.createElement("div")
-bar.id = "bar"
-const bar_inner = document.createElement("div")
-bar_inner.id = "inner"
-bar_inner.style.width = 0
+const bar = document.createElement("div");
+bar.id = "bar";
+const bar_inner = document.createElement("div");
+bar_inner.id = "inner";
+bar_inner.style.width = 0;
 
-bar.appendChild(bar_inner)
+bar.appendChild(bar_inner);
 
-progress.appendChild(progress_tag)
-progress.appendChild(bar)
+progress.appendChild(progress_tag);
+progress.appendChild(bar);
 
-top_info.appendChild(progress)
+top_info.appendChild(progress);
 
-let score = 0
-const round_items = 25
+let score = 0;
 
-const user_score = document.createElement("div")
-user_score.id = "score"
-user_score.className = "info"
+const user_score = document.createElement("div");
+user_score.id = "score";
+user_score.className = "info";
 
-user_score.innerHTML = `<span>Score: <span class='score'>${score}</span></span>`
+user_score.innerHTML = `<span>Score: <span class='score'>${score}</span></span>`;
 
-top_info.appendChild(user_score)
+top_info.appendChild(user_score);
 
 const kana_map = [
 	{
@@ -146,119 +145,123 @@ const kana_map = [
 		name: "wa",
 		kanas: [
 			{ romaji: "wa", hiragana: "わ" },
-			{ romaji: "wi", hiragana: "ゐ" },
+			{ romaji: "_i", hiragana: "ゐ" },
 			{ romaji: "n", hiragana: "ん" },
-			{ romaji: "we", hiragana: "ゑ" },
+			{ romaji: "_e", hiragana: "ゑ" },
 			{ romaji: "wo", hiragana: "を" },
 		]
 	}
-]
+];
 
-let total = 0
+let total = 0;
 for (const row of kana_map) {
 	total += row.kanas.length
 }
-total *= round_items
+
+const shuffle = (kana_map) => {
+	const new_map = structuredClone(kana_map)
+	// Shuffle the kanas in each row.
+	new_map.forEach(row => {
+		let kanas = row.kanas
+		for (let i = kanas.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[kanas[i], kanas[j]] = [kanas[j], kanas[i]]
+		}
+	})
+	// Shuffle the rows in the map.
+	for (let i = new_map.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[new_map[i], new_map[j]] = [new_map[j], new_map[i]];
+	}
+	return new_map
+};
 
 // At the center, we'll show the kana in a big font.
-const kana = document.createElement("div")
-kana.id = "kana"
-kana.innerText = kana_map[0].kanas[3].hiragana
+const kana = document.createElement("div");
+kana.id = "kana";
 
 // At the bottom, we'll place five "buttons" with the romaji that
 // correspond to the row of that kana.
 // I.e. if the kana is "か" the row will contain "ka", "ki", "ku", "ke", "ko".
-const romaji_bar = document.createElement("div")
-romaji_bar.id = "romaji-bar"
+const romaji_bar = document.createElement("div");
+romaji_bar.id = "romaji-bar";
 
-let last_kana = 0
-
-const randomKana = (row, last) => {
-	while (true) {
-		const i = Math.floor(Math.random() * row.length)
-		if (i !== last) {
-			last_kana = i
-			return row[i]
-		}
-	}
-}
+let current = {
+	row: 0,
+	col: 0,
+};
 
 const nextQuest = () => {
-	let row_index = Math.floor(score / round_items)
-	let block = kana_map[row_index % kana_map.length]
-	let row = [...block.kanas]
-	let current_kana = randomKana(row, last_kana)
-	kana.textContent = current_kana.hiragana
+	let kanas = shuffle(kana_map);
+	let block = kanas[current.row];
+	let row = block.kanas;
+	let current_kana = row[current.col];
 
-	if (block.name === "ya") {
-		let filler = { romaji: "", hiragana: "", ignore: true }
-		row = [row[0], filler, row[1], filler, row[2]]
+	current.col++;
+	if (current.col == row.length) {
+		current.col = 0;
+		current.row++;
+	}
+	if (current.row == kanas.length) {
+		current.row = 0;
 	}
 
-	romaji_bar.innerHTML = ""
+	kana.textContent = current_kana.hiragana;
+
+	if (block.name === "ya") {
+		let filler = { romaji: "", hiragana: "", ignore: true };
+		row = [row[0], filler, row[1], filler, row[2]];
+	}
+
+	romaji_bar.innerHTML = "";
 	row.forEach(k => {
-		let btn = document.createElement("div")
-		btn.className = "romaji-button"
-		btn.innerText = k.romaji
+		let btn = document.createElement("div");
+		btn.className = "romaji-button";
+		btn.innerText = k.romaji;
 		if (k.ignore) {
-			btn.classList.add("ignored")
+			btn.classList.add("ignored");
 		} else {
-			btn.addEventListener("click", () => handleClick(k.romaji, current_kana))
+			btn.addEventListener("click", () => handleClick(k.romaji, current_kana));
 			btn.addEventListener('touchstart', () => {
-				btn.classList.add('touched')
-			})
+				btn.classList.add('touched');
+			});
 
 			btn.addEventListener('touchend', () => {
-				btn.classList.remove('touched')
-			})
+				btn.classList.remove('touched');
+			});
 
 			btn.addEventListener('touchcancel', () => {
-				btn.classList.remove('touched')
-			})
+				btn.classList.remove('touched');
+			});
 		}
-		romaji_bar.appendChild(btn)
+		romaji_bar.appendChild(btn);
 	})
 }
 
 const handleClick = (rmj, current) => {
-	const right = rmj === current.romaji
+	const right = rmj === current.romaji;
 	if (right) {
-		score++
-		user_score.innerHTML = `<span>Score: <span class='score'>${score}</span></span>`
-		percentage = Math.min(Math.floor(100 / total * score), 100)
-		progress_tag.innerHTML = `<span>Progress:&nbsp;<span class='perc'>${percentage}%</span></span>`
-		bar_inner.style.width = `${percentage}%`
+		score++;
+		user_score.innerHTML = `<span>Score: <span class='score'>${score}</span></span>`;
+		percentage = Math.min(Math.floor(100 / total * score), 100);
+		progress_tag.innerHTML = `<span>Progress:&nbsp;<span class='perc'>${percentage}%</span></span>`;
+		bar_inner.style.width = `${percentage}%`;
 	}
-	nextQuest()
+	nextQuest();
 }
 
-nextQuest()
+nextQuest();
 // Footer.
-const footer = document.createElement("div")
-footer.id = "footer"
-const footer_content = document.createElement("span")
-footer_content.classList.add("love")
+const footer = document.createElement("div");
+footer.id = "footer";
+const footer_content = document.createElement("span");
+footer_content.classList.add("love");
 // TODO: Add neovim's logo!
-footer_content.innerHTML = "Made in <span class='green'>neo<b>vim</b></span> with 💖 by Nano"
+footer_content.innerHTML = "Made in <span class='green'>neo<b>vim</b></span> with 💖 by Nano";
 
-footer.appendChild(footer_content)
+footer.appendChild(footer_content);
 
-root.appendChild(top_info)
-root.appendChild(kana)
-root.appendChild(romaji_bar)
-root.appendChild(footer)
-
-// Feedback for touched buttons.
-document.querySelectorAll('.romaji-button').forEach(btn => {
-	btn.addEventListener('touchstart', () => {
-		btn.classList.add('touched')
-	})
-
-	btn.addEventListener('touchend', () => {
-		btn.classList.remove('touched')
-	})
-
-	btn.addEventListener('touchcancel', () => {
-		btn.classList.remove('touched')
-	})
-})
+root.appendChild(top_info);
+root.appendChild(kana);
+root.appendChild(romaji_bar);
+root.appendChild(footer);
