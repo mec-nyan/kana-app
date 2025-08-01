424495c0ad6ea88991b9499b910ff22f1e314133
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-08-01 19:37:49 -0300

    First build with Vite.

---
+++ app/static/dist/sw.js
@@ -0,0 +1,68 @@
+const VERSION = "v0.1.0-10";
+
+const CACHE_NAME = `kana-app-${VERSION}`;
+
+const GPATH = "/kana-app";
+
+const APP_STATIC_RESOURCES = [
+	`${GPATH}/`,
+	`${GPATH}/index.html`,
+	`${GPATH}/styles.css`,
+	`${GPATH}/main.js`,
+	`${GPATH}/kanaapp.json`,
+	`${GPATH}/fonts/HackNerdFont-Regular.ttf`,
+	`${GPATH}/fonts/HackNerdFontPropo-Regular.ttf`,
+	`${GPATH}/icons/kapp.png`,
+	`${GPATH}/icons/kapp.svg`,
+	`${GPATH}/screenshots/home.png`,
+	`${GPATH}/screenshots/home-wide.png`
+]
+
+self.addEventListener("install", (e) => {
+	e.waitUntil(
+		(async () => {
+			const cache = await caches.open(CACHE_NAME);
+			cache.addAll(APP_STATIC_RESOURCES);
+		})(),
+	);
+})
+
+self.addEventListener("activate", (e) => {
+	e.waitUntil(
+		(async () => {
+			const names = await caches.keys();
+			await Promise.all(
+				names.map((name) => {
+					if (name !== CACHE_NAME) {
+						return caches.delete(name);
+					}
+					return undefined;
+				}),
+			);
+			await clients.claim();
+		})(),
+	);
+});
+
+self.addEventListener("fetch", (e) => {
+	if (e.request.mode === "navigate") {
+		e.respondWith(caches.match(`${GPATH}/`));
+		return;
+	}
+
+	e.respondWith(
+		(async () => {
+			const cache = await caches.open(CACHE_NAME);
+			const cachedResponse = await cache.match(e.request.url);
+			if (cachedResponse) {
+				return cachedResponse;
+			}
+			try {
+				const resp = await fetch(e.request);
+				return resp;
+			} catch (err) {
+				return new Response(null, { status: 404 });
+			}
+		})(),
+	);
+});
