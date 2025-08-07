5adadab29f7899a01017e84407977984a225b872
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-08-06 21:44:03 -0300

    Move kanas to its own file.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import { registerServiceWorker } from "./service-worker";
 import { DevInfoOverlay } from "./developer-info";
+import { kanaMap } from "./kanas";
 
 registerServiceWorker();
 
-// We'll be manipulating this div.
-const root = document.getElementById("root");
-
 // Show window size in development mode.
 const info = new DevInfoOverlay();
+
+
+// We'll be manipulating this div.
+const root = document.getElementById("root");
 
 // We'll have a header, a content-div and a footer.
 // The center pane will contain actions (home) and kana/buttons (game).
 const center_pane = document.createElement("div");
 center_pane.id = "center-pane";
-
 
 
 // Handle dark/light themes.
@@ -197,110 +198,6 @@ const tip = document.createElement("div");
 tip.id = "tip";
 tip.innerHTML = "<em>Tip: touch the hiragana to listen again.</em>"
 
-const duration = 0.56;
-
-// Hiragana chart.
-const kana_map = [
-	{
-		name: "a",
-		kanas: [
-			{ romaji: "a", hiragana: "あ", start: 0.0, duration: duration },
-			{ romaji: "i", hiragana: "い", start: 0.4, duration: duration },
-			{ romaji: "u", hiragana: "う", start: 0.9, duration: duration },
-			{ romaji: "e", hiragana: "え", start: 1.5, duration: duration },
-			{ romaji: "o", hiragana: "お", start: 2.0, duration: duration },
-		],
-	},
-	{
-		name: "ka",
-		kanas: [
-			{ romaji: "ka", hiragana: "か", start: 2.6, duration: duration },
-			{ romaji: "ki", hiragana: "き", start: 3.2, duration: duration },
-			{ romaji: "ku", hiragana: "く", start: 3.8, duration: duration },
-			{ romaji: "ke", hiragana: "け", start: 4.4, duration: duration },
-			{ romaji: "ko", hiragana: "こ", start: 5.0, duration: duration },
-		]
-	},
-	{
-		name: "sa",
-		kanas: [
-			{ romaji: "sa", hiragana: "さ", start: 5.6, duration: duration },
-			{ romaji: "shi", hiragana: "し", start: 6.0, duration: duration },
-			{ romaji: "su", hiragana: "す", start: 6.7, duration: duration },
-			{ romaji: "se", hiragana: "せ", start: 7.4, duration: duration },
-			{ romaji: "so", hiragana: "そ", start: 8.0, duration: duration },
-		]
-	},
-	{
-		name: "ta",
-		kanas: [
-			{ romaji: "ta", hiragana: "た", start: 8.6, duration: duration },
-			{ romaji: "chi", hiragana: "ち", start: 9.2, duration: duration },
-			{ romaji: "tsu", hiragana: "つ", start: 9.8, duration: duration },
-			{ romaji: "te", hiragana: "て", start: 10.4, duration: duration },
-			{ romaji: "to", hiragana: "と", start: 11.0, duration: duration },
-		]
-	},
-	{
-		name: "na",
-		kanas: [
-			{ romaji: "na", hiragana: "な", start: 11.6, duration: duration },
-			{ romaji: "ni", hiragana: "に", start: 12.1, duration: duration },
-			{ romaji: "nu", hiragana: "ぬ", start: 12.8, duration: duration },
-			{ romaji: "ne", hiragana: "ね", start: 13.4, duration: duration },
-			{ romaji: "no", hiragana: "の", start: 14.0, duration: duration },
-		]
-	},
-	{
-		name: "ha",
-		kanas: [
-			{ romaji: "ha", hiragana: "は", start: 14.6, duration: duration },
-			{ romaji: "hi", hiragana: "ひ", start: 15.2, duration: duration },
-			{ romaji: "fu", hiragana: "ふ", start: 15.8, duration: duration },
-			{ romaji: "he", hiragana: "へ", start: 16.5, duration: duration },
-			{ romaji: "ho", hiragana: "ほ", start: 17.0, duration: duration },
-		]
-	},
-	{
-		name: "ma",
-		kanas: [
-			{ romaji: "ma", hiragana: "ま", start: 17.7, duration: duration },
-			{ romaji: "mi", hiragana: "み", start: 18.3, duration: duration },
-			{ romaji: "mu", hiragana: "む", start: 18.9, duration: duration },
-			{ romaji: "me", hiragana: "め", start: 19.4, duration: duration },
-			{ romaji: "mo", hiragana: "も", start: 20.0, duration: duration },
-		]
-	},
-	{
-		name: "ya",
-		kanas: [
-			{ romaji: "ya", hiragana: "や", start: 20.6, duration: duration },
-			{ romaji: "yu", hiragana: "ゆ", start: 21.1, duration: duration },
-			{ romaji: "yo", hiragana: "よ", start: 21.7, duration: duration },
-		]
-	},
-	{
-		name: "ra",
-		kanas: [
-			{ romaji: "ra", hiragana: "ら", start: 22.3, duration: duration },
-			{ romaji: "ri", hiragana: "り", start: 23.0, duration: duration },
-			{ romaji: "ru", hiragana: "る", start: 23.5, duration: duration },
-			{ romaji: "re", hiragana: "れ", start: 24.1, duration: duration },
-			{ romaji: "ro", hiragana: "ろ", start: 24.7, duration: duration },
-		]
-	},
-	{
-		name: "wa",
-		kanas: [
-			{ romaji: "wa", hiragana: "わ", start: 25.3, duration: duration },
-			{ romaji: "i", hiragana: "ゐ", start: 0.4, duration: duration },
-			{ romaji: "n", hiragana: "ん", start: 25.9, duration: duration },
-			{ romaji: "e", hiragana: "ゑ", start: 1.5, duration: duration },
-			{ romaji: "wo", hiragana: "を", start: 26.4, duration: duration },
-		]
-	},
-];
-
 
 // At the center, we'll show the kana in a big font.
 const kana = document.createElement("div");
@@ -334,10 +231,10 @@ function count_kanas(game) {
 // Populate the kanas for this drill.
 // In "dev" mode, use less rows to play a quick round.
 function make_game(dev = false) {
-	let game_map = [...kana_map];
+	let game_map = [...kanaMap];
 	if (dev) {
 		// Try out sound, let's just use the first five kanas.
-		game_map = [kana_map[0]];
+		game_map = [kanaMap[0]];
 	}
 	game_map.forEach(row => {
 		let game_row = {
