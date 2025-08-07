3c7df94c832a183c4441ec4d016af3652b5bdca5
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-08-06 21:11:37 -0300

    Move service worker registration to its own file.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
-// Register service worker.
-if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("sw.js").then(
-		(registration) => {
-			console.log("Service worker registered:", registration);
-		},
-		(error) => {
-			console.error(`Service worker registration failed: ${error}`);
-		}
-	);
-} else {
-	console.error("Service workers are not suppported.");
-}
+import { registerServiceWorker } from "./service-worker";
+registerServiceWorker();
 
 // We'll be manipulating this div.
 const root = document.getElementById("root");
