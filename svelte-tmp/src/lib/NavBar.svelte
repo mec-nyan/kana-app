5d2199c992a55f1c877ae0593fb9876655d05b7f
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-08-22 21:09:43 -0300

    Add nav component.

---
+++ svelte-tmp/src/lib/NavBar.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	let { title } = $props();
+</script>
+
+<div id="nav">
+	<div id="menu">󰍜</div>
+	<h3>{title}</h3>
+	<label id="theme-sel" for=""></label>
+</div>
+
+<style lang="scss">
+	#nav {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+
+		width: 100%;
+		height: 4rem;
+		padding: 0 1rem;
+
+		font-family: 'Hack Nerd Font';
+
+		background: var(--mantle);
+		border-bottom: 1px solid var(--surface0);
+
+		/* border: 1px solid red; */
+	}
+
+	#nav h3 {
+		font-size: 1rem;
+		color: var(--blue);
+
+		/* border: 1px solid blue; */
+	}
+
+	#menu,
+	#theme-sel {
+		aspect-ratio: 1/1;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 1.6rem;
+		color: var(--surface2);
+		cursor: pointer;
+
+		/* border: 1px solid green; */
+	}
+</style>
--- svelte-tmp/src/routes/+page.svelte
+++ svelte-tmp/src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import Root from "$lib/Root.svelte";
-	import TopContainer from "$lib/TopContainer.svelte";
-	import Termscreen from "$lib/Termscreen.svelte";
-	import MainPane from "$lib/MainPane.svelte";
-	import ActionsPane from "$lib/ActionsPane.svelte";
-	import OptionButtons from "$lib/OptionButtons.svelte";
-	import Start from "$lib/Start.svelte";
-	import Footer from "$lib/Footer.svelte";
+	import Root from '$lib/Root.svelte';
+	import TopContainer from '$lib/TopContainer.svelte';
+	import Termscreen from '$lib/Termscreen.svelte';
+	import MainPane from '$lib/MainPane.svelte';
+	import ActionsPane from '$lib/ActionsPane.svelte';
+	import OptionButtons from '$lib/OptionButtons.svelte';
+	import Start from '$lib/Start.svelte';
+	import Footer from '$lib/Footer.svelte';
 
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
+	import NavBar from '$lib/NavBar.svelte';
 
 	let height = 0;
 	let width = 0;
@@ -20,9 +21,7 @@
 </script>
 
 <Root>
-	<TopContainer>
-		<Termscreen />
-	</TopContainer>
+	<NavBar  title="Kana App!"/>
 
 	<MainPane {height} {width}>
 		<ActionsPane>
@@ -30,6 +29,10 @@
 			<Start />
 		</ActionsPane>
 	</MainPane>
+
+	<TopContainer>
+		<Termscreen />
+	</TopContainer>
 
 	<Footer />
 </Root>
